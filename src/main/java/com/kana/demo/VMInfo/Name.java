12ebed9db3c4de842bb0af106b61c277c319b907
arke <arke@localmail.com>
2015-02-17 17:29:01 +0700

    tidy up comments

--- src/main/java/com/kana/demo/VMInfo/Name.java
+++ src/main/java/com/kana/demo/VMInfo/Name.java
@@ -29,14 +29,8 @@ public class Name {
 	public static void main(String[] args) throws RemoteException, MalformedURLException, UnknownHostException, SocketException {
 
         //API Initialization
-//		long start = System.currentTimeMillis();
 	    ServiceInstance si = new ServiceInstance(new URL("https://10.77.28.2/sdk"), "root", "badjmac2", true);
-
-//	    long end = System.currentTimeMillis();
-//	    System.out.println("time taken:" + (end-start));
 	    Folder rootFolder = si.getRootFolder();
-//	    String name = rootFolder.getName();
-//	    System.out.println("root:" + name);
 	    ManagedEntity[] mes = new InventoryNavigator(rootFolder).searchManagedEntities("VirtualMachine");
 	    if(mes==null || mes.length ==0)
 	    {
@@ -60,52 +54,29 @@ public class Name {
                     localIP=i.getHostAddress();
             }
         }
-        System.out.println("Local IP = " + localIP);
 
         //comparing localIP var to machines IP in ESXi
-//        ArrayList<VirtualMachine> vms = new ArrayList<VirtualMachine>();
-
-//            vms = esxi.getVirtualMachines();
         boolean match = false;
         String VMNameOnESXi="";
-            for (int i = 0; i < mes.length; i++) {
-//                ManagedVirtualMachine mn = null;
-                try {
-                    VirtualMachine vm = (VirtualMachine) mes[i];
-                    String  ipVMinESXi=vm.getGuest().getIpAddress();
-                    if(ipVMinESXi!=null) {
-                        if (ipVMinESXi.equals(localIP)) {
-                            VMNameOnESXi=vm.getName();
-                            match=true;
-                        }
+        for (int i = 0; i < mes.length; i++) {
+            try {
+                VirtualMachine vm = (VirtualMachine) mes[i];
+                String  ipVMinESXi=vm.getGuest().getIpAddress();
+                if(ipVMinESXi!=null) {
+                    if (ipVMinESXi.equals(localIP)) {
+                        VMNameOnESXi=vm.getName();
+                        match=true;
                     }
-                } catch (Exception ex) {
-                    ex.printStackTrace();
                 }
+            } catch (Exception ex) {
+                ex.printStackTrace();
             }
+        }
         if (match) {
             System.out.println(VMNameOnESXi);
         } else {
             System.out.println("This machine is not hosted on ESXi!");
         }
-//        VirtualMachine vms = (VirtualMachine) mes[10];
-//
-//
-//		VirtualMachineConfigInfo vminfo = vms.getConfig();
-//	    VirtualMachineCapability vmc = vms.getCapability();
-//
-//		GuestInfo ss = vms.getGuest();
-//
-//		vms.getResourcePool();
-//	    System.out.println("Hello " + vms.getName());
-//
-//		System.out.println("Hello my IP Address " + vms.getGuest().getIpAddress());
-//
-//		System.out.println("Hello locationID = " + vms.getConfig().getLocationId());
-//
-//		System.out.println("GuestOS: " + vminfo.getGuestFullName());
-//	    System.out.println("Multiple snapshot supported: " + vmc.isMultipleSnapshotsSupported());
-//	    System.out.println("GuestOS: " + vminfo);
 
 	    si.getServerConnection().logout();
 	}
